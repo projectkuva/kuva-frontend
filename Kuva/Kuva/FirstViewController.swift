96c4c6fe3735c679db8cc56fa4dcd548f5561a99
Matthew Ess <mess@purdue.edu>
2017-01-30 16:28:47 -0500

    Add skeleton iOS project files

---
+++ Kuva/Kuva/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kuva
+//
+//  Created by Matthew on 1/30/17.
+//  Copyright © 2017 kuva. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
