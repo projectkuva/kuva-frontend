59009533f116812a48faecaad05944bb8ea65e56
Shane DeWael <dewaels@purdue.edu>
2017-02-03 15:09:34 -0500

    signin

--- Kuva/Kuva/ViewController.swift
+++ Kuva/Kuva/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func signinPressed(_ sender: Any) {
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+        
+        //use AlamoFire to login with backend
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
