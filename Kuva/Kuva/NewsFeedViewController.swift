51577dda231dd5c168859007ad6917317fad1fcd
Shane DeWael <dewaels@purdue.edu>
2017-02-28 14:10:40 -0500

    add recent activity

---
+++ Kuva/Kuva/NewsFeedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsFeedViewController.swift
+//  
+//
+//  Created by Shane DeWael on 2/28/17.
+//
+//
+
+import UIKit
+
+class NewsFeedViewController: PrimaryViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
